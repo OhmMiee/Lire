c224eb0420577c1c8ce9d4a6490431e6b5c30ca7
Jeraphan Boonsin <jeraphan2000@gmail.com>
2021-07-22 20:34:13 +0700

    connect database

--- app.py
+++ app.py
@@ -1,9 +1,22 @@
 from flask import Flask, render_template
+import pymysql
+
 app = Flask(__name__)
-	
+
+# database
+conn = pymysql.connect( host='localhost',
+                        user='root',
+                        password='',
+                        database='audiobooks',)
+
 @app.route('/')
 def Home():
-   return render_template('index.html')
+   time = "5.00"
+   return render_template('index.html', data=time)
+
+@app.route('/audiobook')
+def audiobook_page():
+   return "Hi"
 
 if __name__ == "__main__":
     app.run(debug=True)
