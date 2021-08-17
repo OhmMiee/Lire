88ce8ee4000f22852168c05b8ba6da112090e9d8
tarssrch9 <ppp0981907990@gmail.com>
2021-08-17 22:42:13 +0700

    update app.py

--- app.py
+++ app.py
@@ -1,25 +1,44 @@
-from flask import Flask, render_template
-# import pymysql
+from flask import Flask, render_template,request,redirect,url_for
+import pymysql
 
 app = Flask(__name__)
 
 # database
-# conn = pymysql.connect( host='localhost',
-#                         user='root',
-#                         password='',
-#                         database='audiobooks',)
+conn = pymysql.connect( host='localhost',
+                        user='root',
+                        password='',
+                        database='audiobooks',)
 
-# @app.route('/')
-# def Home():
-#    with conn:
-#       cur = conn.cursor()
-#       cur.execute('select book_title, author, date_format(time,"%i:%s") as Minutes, book_img from books')
-#       rows = cur.fetchall()
-#       return render_template('index.html', datas=rows)
+@app.route('/')
+def Home():
+   with conn:
+      cur = conn.cursor()
+      cur.execute('select book_title, author, date_format(time,"%i:%s") as Minutes, book_img from books')
+      rows = cur.fetchall()
+      return render_template('index.html', datas=rows)
 
 @app.route('/audiobook')
 def audiobook_page():
    return render_template('admin.html')
+
+@app.route('/upload')
+def upload_page():
+
+@app.route('/insert', methods=['POST'])
+def insert():
+   if request.method=="POST":
+      category=request.form['category']
+      imagesfile=request.form['file']
+      bookfile=request.form['bookfile']
+      Bookname=request.form['Bookname']
+      Authorname=request.form['Authorname']
+      story=request.form['story']
+     
+      with conn.cursor() as cursor:
+         sql="insert into 'books'('category','book_img','book_file','book_name','Author','story') values(%s,%s,%s,%s,%s,%s)"
+         cursor.execute(sql,(category, imagesfile,  bookfile,Bookname , Authorname,story))
+         conn.commit()
+      return redirect(url_for('upload'))
 
 if __name__ == "__main__":
     app.run(debug=True) 
