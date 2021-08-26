9404cf5309f539a69a4c68183b7c2115db901a17
Jeraphan Boonsin <jeraphan2000@gmail.com>
2021-08-26 16:21:14 +0700

    ohm edit

--- app.py
+++ app.py
@@ -1,4 +1,7 @@
-from flask import Flask, render_template,request,redirect,url_for
+from flask import Flask, render_template,request,redirect,url_for, flash
+from datetime import datetime
+from werkzeug.utils import secure_filename
+import os
 import pymysql
 
 app = Flask(__name__)
@@ -7,7 +10,18 @@ app = Flask(__name__)
 conn = pymysql.connect( host='localhost',
                         user='root',
                         password='',
-                        database='audiobooks',)
+                        database='testingdb',)
+
+UPLOAD_FOLDER = 'static/uploads/'
+ 
+app.secret_key = "secret key"
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+app.config['MAX_CONTENT_LENGTH'] = 16 * 1024 * 1024
+
+ALLOWED_EXTENSIONS = set(['png', 'jpg', 'jpeg', 'gif'])
+ 
+def allowed_file(filename):
+    return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS  
 
 @app.route('/')
 def Home():
@@ -28,19 +42,42 @@ def upload_page():
 @app.route('/insert', methods=['POST'])
 def insert():
    if request.method == "POST":
+      
+
+      if 'imagefile' not in request.files:
+         flash('No file part')
+         return redirect(request.url)
+
+      imagefile = request.files['imagefile']
+      now = datetime.now()
+
+      if imagefile.filename == '':
+         flash('No image selected for uploading')
+         return redirect(request.url)
+      
+
+      if imagefile and allowed_file(imagefile.filename):
+         filename = secure_filename(imagefile.filename)
+         imagefile.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+      
+         #print('upload_image filename: ' + filename)
+         flash('Image successfully uploaded and displayed below')
+         return filename
+
       category=request.form['category']
-      imagefile=request.form['imagefile']
-      bookfile=request.form['bookfile']
       bookname=request.form['bookname']
       authorname=request.form['authorname']
       story=request.form['story']
-     
       with conn.cursor() as cursor:
          sql="insert into 'books'('category','book_img','book_file','book_name','Author','story') values(%s,%s,%s,%s,%s,%s)"
          cursor.execute(sql,(category, imagefile, bookfile, bookname , authorname, story))
          conn.commit()
-   
-   return redirect(url_for('upload'))
 
+   
+   
+   
+   
+
+   
 if __name__ == "__main__":
     app.run(debug=True) 
